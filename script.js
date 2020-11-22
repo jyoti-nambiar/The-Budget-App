c455ad68a37adf3008615f19c703578c91c69464
Jyoti <jyoti.nambiar@medieinstitutet.se>
2020-11-22 17:38:30 +0100

    added comments and made changes to the media queries

--- script.js
+++ script.js
@@ -7,26 +7,34 @@ function addItems(e) {
     e.preventDefault();
 
     var selectOption = document.querySelector("#chooseOption").value;
+
+    /*the select tab's value is evaluated to see if it has a value of "+ "or "- "
+    based on the selection the value is added to the income's tab or to the 
+    
+    */
     if (selectOption === "+") {
         var description = document.querySelector("#description");
         var amount = document.querySelector("#amount");
+
+        if (!description.value || !amount.value) return; //check for null value 
         var income = {
             type: "income",
             title: description.value,
             sum: Number(amount.value)
         };
-        entryList.push(income);
+        entryList.push(income);  //pushing the object to the array
         var totalInkomster = document.querySelector(".inkomsterSpan");
-        totalInkomster.textContent = calculateTotal("income", entryList);
+        totalInkomster.textContent = calculateTotal("income", entryList); //calling function to display the total for inkomster
 
         var createNewLi = document.createElement("li");
-        createNewLi.textContent = income.title + ":" + income.sum;
+        createNewLi.textContent = income.title + ":" + income.sum;  // arrayList display
         document.querySelector(".inkomstList").appendChild(createNewLi);
         clearText([description, amount]);
 
     } else if (selectOption === "-") {
         var description = document.querySelector("#description");
         var amount = document.querySelector("#amount");
+        if (!description.value || !amount.value) return; // checking for null values
         var expense = {
             type: "expense",
             title: description.value,
@@ -34,7 +42,7 @@ function addItems(e) {
         };
         entryList.push(expense);
         var totalExpense = document.querySelector(".konstnadSpan")
-        totalExpense.textContent = calculateTotal("expense", entryList);
+        totalExpense.textContent = calculateTotal("expense", entryList);//calling function to display the total for kostnad 
 
         var createNewLi = document.createElement("li");
         createNewLi.textContent = expense.title + ":" + expense.sum;
@@ -45,11 +53,11 @@ function addItems(e) {
     var income = calculateTotal("income", entryList);
     var expense = calculateTotal("expense", entryList);
 
-    document.querySelector(".SparandeSpan").textContent = (income - expense);
+    document.querySelector(".SparandeSpan").textContent = (income - expense); // display the sparande after subtracting the total inkomster from total kostnad
 
 
 }
-
+// function declaration for clearing the input values after the button "Lägg till" is clicked
 function clearText(arrayList) {
     arrayList.forEach(entry => {
         entry.value = "";
@@ -58,6 +66,7 @@ function clearText(arrayList) {
 
 }
 
+//function for calculating the total of inkomster, kostnad and sparande
 function calculateTotal(type, entryList) {
     var sum = 0;
     entryList.forEach(entry => {
@@ -68,7 +77,7 @@ function calculateTotal(type, entryList) {
     return sum;
 
 };
-
+// adding event listner for clear function
 document.querySelector("#clear").addEventListener("click", clearFunction)
 
 
